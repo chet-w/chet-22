d6f6a5f9a8ff226eb6348f0bf906faaa333cc461
Chet Davidson <chethana96@gmail.com>
2022-02-12 11:25:01 +1300

    Create new Paragraph component for fleshing out the About section
    This change creates a new paragrph component which is styled and used
    to build out the About component since it is just text for the most
    part.

--- components/sections/About/About.tsx
+++ components/sections/About/About.tsx
@@ -1,14 +1,29 @@
 import React, { Fragment, ReactElement } from "react";
 import { Section } from "../../layout/Section";
+import { Accent } from "../../typography/Accent/styles";
+import { Heading } from "../../typography/Headings";
+import { Paragraph } from "../../typography/Paragraph";
 
 export function About(): ReactElement {
   return (
     <Fragment>
-      <Section
-        direction="horizontal"
-        crossAxis="start"
-        mainAxis="center"
-      ></Section>
+      <Section direction="vertical" crossAxis="start" mainAxis="start">
+        <Heading>
+          <Accent>About me</Accent>
+        </Heading>
+        <Paragraph>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore
+          consequatur libero blanditiis cum, consequuntur sint nobis ea
+          voluptatem at perspiciatis obcaecati eveniet consectetur saepe tempore
+          ex est optio? Inventore, eveniet.
+        </Paragraph>
+        <Paragraph>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore
+          consequatur libero blanditiis cum, consequuntur sint nobis ea
+          voluptatem at perspiciatis obcaecati eveniet consectetur saepe tempore
+          ex est optio? Inventore, eveniet.
+        </Paragraph>
+      </Section>
     </Fragment>
   );
 }
---
+++ components/typography/Paragraph/styles.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Paragraph = styled.p`
+  margin: 0 0 1rem 0;
+  max-width: 60ch;
+  line-height: 1.25;
+`;
